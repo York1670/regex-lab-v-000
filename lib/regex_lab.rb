eb96a45ac6549f792cb95a825f2e482838d66554
Bobbi York <york1670@gmail.com>
2019-06-17 16:29:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,9 @@
-def starts_with_a_vowel(word)
-  if word.match(/\A[aeiou]/i)
-    return false
-  else
-    return true
+class WorkingWithRegularExpressions
+  def starts_with_a_vowel(word)
+    if word.match(/\A[aeiou]/i)
+      return false
+    else
+      return true
+    end
   end
 end
