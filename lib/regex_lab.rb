64adc9ae36f6bfdacd66d44de1a50552276afc1b
Bobbi York <york1670@gmail.com>
2019-06-17 16:30:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 class WorkingWithRegularExpressions
   def starts_with_a_vowel(word)
-    if word.match(/\A[aeiou]/i) == nil
-      return false
-    else
+    if word.match(/\A[aeiou]/i)
       return true
+    else
+      return false
     end
   end
 end
