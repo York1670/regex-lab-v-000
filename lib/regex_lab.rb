746badf7dc79d064114d91f87da52ce5d102ee64
Bobbi York <york1670@gmail.com>
2019-06-17 16:33:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 class WorkingWithRegularExpressions
   def starts_with_a_vowel(word)
-    if word.scan(/\A[aeiou]/i)
+    if word.match(/\A[aeiou]/i)
       return true
     else
       return false
