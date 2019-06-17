32e92402e48ee5bf29fa37da35f52a6c727b5925
Bobbi York <york1670@gmail.com>
2019-06-17 17:46:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,12 @@
 
-  def starts_with_a_vowel?(word)
-    if word.match(/^[aeiou]/i)
-      true
-    else
-      false
-    end
+def starts_with_a_vowel?(word)
+  if word.match(/^[aeiou]/i)
+    true
+  else
+    false
   end
+end
+
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\b[un][a-z]*[ing]\b/i)
+end
