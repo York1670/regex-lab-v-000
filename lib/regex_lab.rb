20119ec021a1c51eb5bb2599936dae975abd9ca7
Bobbi York <york1670@gmail.com>
2019-06-17 17:58:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,3 +14,12 @@ end
 def words_five_letters_long(words)
   words.scan(/\b\w{5}\b/)
 end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(^[A-Z].+$\W)
+    return true
+  else
+    return false
+  end
+end
+ß
