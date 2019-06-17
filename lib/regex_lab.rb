c4ea954193090abe66aedada5b742b2a156c47c8
Bobbi York <york1670@gmail.com>
2019-06-17 18:04:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,7 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(numbers)
-  if numbers.scan (/\d/).length == 10
+  if numbers.scan(/\d/).length == 10
     return true
   else
     return false
